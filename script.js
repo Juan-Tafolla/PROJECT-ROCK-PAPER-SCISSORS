f29841220d51eec9f92d0d62d9c096e865609bee
Juan-Tafolla <juantafolla2001@gmail.com>
2022-07-11 21:28:38 -0500

    Finished program

--- script.js
+++ script.js
@@ -2,10 +2,43 @@
 function computerPlay(){
 
   let x = Math.floor(Math.random() * 3 + 1);
-  let message = (x < 2) ? 'Rock!' :
-    (x < 3) ? 'Paper!' :
-    'Scissors!';
+  let message = (x < 2) ? 'rock' :
+    (x < 3) ? 'paper' :
+    'scissors';
 
  return message;
     
+}
+
+function userPlay(){
+  let x = window.prompt("Rock, paper, scissors! Go!: ");
+  let y = x.toLowerCase(); 
+  return y;
+}
+
+function playRound(playerSelection,computerSelection){
+  console.log(`You choosed: ${playerSelection}`)
+  console.log(`The PC choosed: ${computerSelection}`)
+  
+  if (playerSelection == computerSelection){
+    message = 'ITS A DRAAW!';
+  }else if((playerSelection == 'rock') && (computerSelection == 'scissors')){
+    message = 'YOU WIN!';
+  }else if((playerSelection == 'rock') && (computerSelection == 'paper')){
+    message = 'YOU LOSE!';
+  }else if((playerSelection == 'paper') && (computerSelection == 'rock')){
+    message = 'YOU WIN!';
+  }else if((playerSelection == 'paper') && (computerSelection == 'scissors')){
+    message = 'YOU LOSE!';
+  }else if((playerSelection == 'scissors') && (computerSelection == 'rock')){
+    message = 'YOU LOSE!';
+  }else if((playerSelection == 'scissors') && (computerSelection == 'paper')){
+    message = 'YOU WIN!';
+  }
+  return message;
+}
+function main(){
+const playerSelection = userPlay();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection,computerSelection));
 }
